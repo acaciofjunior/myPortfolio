88746173633b070ac62496569b3d7d956c4e89ef
Acacio Fernandes Jr <acaciofernandesjr@yahoo.com.br>
2024-03-06 16:13:08 -0300

    delete api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 
+async function fetchProfileData() {
+    const url = '../../data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+}
+
+
 function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
